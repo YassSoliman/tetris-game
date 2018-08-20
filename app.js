70306b0ead4da9453e86d1dfabbe9428b7295bc2
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-08-19 23:00:39 -0400

    Added drawing functions

--- app.js
+++ app.js
@@ -23,5 +23,40 @@ function update() {
     // check logic
                         
     // draw everything
+    drawBlock('I', 10, 10);  
+    drawBlock('J', 30, 30);
     console.log('Frame');
+}
+
+// draw a specific block
+function drawBlock(type, x, y){
+  switch(type){
+    case 'I':
+      drawRect(x, y, '#0092ff');
+      break;
+		case 'J':
+      drawRect(x, y , '#ff0000');
+      break;
+  }
+}
+
+// draw a rectangle with or without a stroke
+function drawRect(x, y, width, height, rectColor, wantsStroke, strokeColor){
+  ctx.fillStyle = rectColor;
+	ctx.fillRect(x, y, width, height);
+  if(wantsStroke){
+    ctx.strokeStyle = strokeColor;
+		ctx.strokeRect(x, y, width, height);
+  }
+}
+
+// draw one unit
+function drawUnit(x, y, color){
+  var size = 3;
+	drawRect(x, y, UNIT, UNIT, color, false); // Draw main color
+	drawRect(x+UNIT-size, y, size, UNIT, 'black', false); // Draw right black border
+	drawRect(x, y+UNIT-size, UNIT, size, 'black', false); // Draw left black border
+	drawRect(x, y, size, size, 'white', false); // Draw top right white dot
+	drawRect(x+size, y+size, UNIT, UNIT*2, 'white', false); // Draw top right top border
+	drawRect(x+size, y+size, UNIT*2, UNIT, 'white', false); // Draw top right left border 
 }

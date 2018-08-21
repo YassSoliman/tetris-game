f44149fabf20055de5cd914771461283f9e81a7b
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-08-20 22:30:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ function update() {
     // check logic
                         
     // draw everything
-    drawBlock('I', 10, 10);  
-    drawBlock('J', 35, 35);
+
 }
 
 // One function that calculates position, rotation direction of any piece
@@ -63,19 +62,113 @@ var S = {
         color: '#00ff92'
 }; 
 var T = { 
-  	blocks: [0x4E00, 0x4640, 0x0E40, 0x4C40],
-        color: '#ffdb00'
-}; 									
+  	blocks: [0xA5A5, 0x0F00, 0x4444, 0x00F0],
+        color: '#0092ff'
+}; 
 var Z = { 
-  	blocks: [0xC600, 0x2640, 0x0C60, 0x4C80],
-        color: '#ff00db'
-};
+	blocks: [0xA5A5, 0x0F00, 0x4444, 0x00F0],
+        color: '#0092ff'
+}; 
+
+
+// Rotations go from 0 - 3
+// rotations are I.blocks[rotationIndex];
+// Vertical -- 0
 //  8 4 2 1
-//[ X X O O		1000		0x4000  
-//  O X X O	  100			0x0200   0x8000 + 0x4000 + 0x0400 = 0xC600
-//  O O O O   10			0x0040
+//[ O O X O		1000		0x2000
+//  O O X O	  100			0x0200
+//  O O X O   10			0x0020
+//  O O X O ] 1				0x0002
+//										0x2222
+
+// 0x8000 --> 0x4000 --> 0x2000 --> 0x1000 --> 0x0800 -->  0x0400 --> 0x0200 --> 0x0100 --> 0x0080 --> 0x0040 --> 0x0020 --> 0x0010
+
+// Horizontal -- 1
+//  8 4 2 1
+//[ O O O O		1000		0x0000
+//  X X X X	  100			0x0800 + 0x0400 + 0x0200 + 0x0100 = 0x0F00
+//  O O O O   10			0x0000
 //  O O O O ] 1				0x0000
-//	
+//										0x0F00
+
+// Vertical -- 2
+//  8 4 2 1
+//[ O X O O		1000		0x4000
+//  O X O O	  100			0x0400
+//  O X O O   10			0x0040
+//  O X O O ] 1				0x0004
+//										0x4444
+
+// Horizontal -- 3
+//  8 4 2 1
+//[ O O O O		1000		0x0000
+//  O O O O	  100			0x0000 
+//  X X X X   10			0x0080 + 0x0040 + 0x0020 + 0x0010 = 0x00F0
+//  O O O O ] 1				0x0000
+//										0x0000
+
+//  8 4 2 1
+//[ O 1 1 O		1000		0x0000
+//  O 1 1 O	  100			0x0000 
+//  O O O O   10			0x0000+ 0x4000 + 0x2000 + 0x0400 + 0x0200 = 0x6600
+//  O O O O ] 1				0x0000
+//										0x0000
+
+// draw O block
+function drawO(x, y){
+  drawUnit(x, y , '#4900ff');
+  drawUnit(x+UNIT, y, '#4900ff');
+  drawUnit(x+UNIT, y+UNIT , '#4900ff');
+  drawUnit(x, y+UNIT , '#4900ff');
+}
+
+// draw L block
+function drawL(x, y){
+  drawUnit(x, y , '#49ff00');
+  drawUnit(x, y-UNIT, '#49ff00');
+  drawUnit(x+UNIT, y-UNIT , '#49ff00');
+  drawUnit(x+UNIT*2, y-UNIT , '#49ff00');
+}
+
+// draw J block
+function drawJ(x, y){
+  drawUnit(x, y , '#ff0000');
+  drawUnit(x+UNIT, y , '#ff0000');
+  drawUnit(x+UNIT*2, y , '#ff0000');
+  drawUnit(x+UNIT*2, y+UNIT , '#ff0000');
+}
+
+// draw I block
+function drawI(x, y){
+	drawUnit(x, y, '#0092ff');
+	drawUnit(x+UNIT, y, '#0092ff');
+	drawUnit(x+UNIT*2, y, '#0092ff');
+	drawUnit(x+UNIT*3, y, '#0092ff');
+}
+
+//draw S block
+function drawS(x, y){
+  drawUnit(x, y, '#00ff92');
+  drawUnit(x-UNIT, y, '#00ff92');
+  drawUnit(x, y+UNIT, '#00ff92');
+  drawUnit(x+UNIT, y+UNIT, '#00ff92');
+}
+
+//draw T block
+function drawT(x,y){
+  drawUnit(x, y, '#ffdb00');
+  drawUnit(x, y-UNIT, '#ffdb00');
+  drawUnit(x+UNIT, y, '#ffdb00');
+  drawUnit(x-UNIT, y, '#ffdb00');
+}
+
+//draw Z block
+function drawZ(x,y){
+  drawUnit(x, y, '#ff00db');
+  drawUnit(x, y+UNIT, '#ff00db');
+  drawUnit(x+UNIT, y, '#ff00db');
+  drawUnit(x-UNIT, y+UNIT, '#ff00db');
+}
 
 // draw a rectangle with or without a stroke
 function drawRect(x, y, width, height, rectColor, wantsStroke, strokeColor){
